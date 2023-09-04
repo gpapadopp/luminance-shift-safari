ac507bd25ca97faab6d89fb3bbb11e28ccb33818
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-09-05 01:46:52 +0300

    Initial Commit

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,13 @@
-console.log("Hello World!", browser);
+// popup.js
+
+function updatePopupContent() {
+  browser.extension.getLocalizedUserMessage("extension_name", function(message) {
+      console.log(message)
+//    document.getElementById("welcomeMessage").textContent = message;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  updatePopupContent();
+});
+
