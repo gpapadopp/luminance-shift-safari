c34b1d31728d07c944f0ffd3ae25b664a131651f
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-09-05 21:40:02 +0300

    Added reading mode overlay to webpage on switch toggle.

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,35 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+function displayOverlay(){
+    const readingOverlay = document.createElement("div")
+    readingOverlay.id = "lss_reading_mode_overlay"
+    readingOverlay.style = "position: fixed; width: 100%; height: 100%; top: 0; left: 0; right:0; bottom: 0; background-color: rgba(255, 210, 124, 0.21); z-index: 2147483647;"
+    document.body.appendChild(readingOverlay)
+}
+
+function destroyOverlay(){
+    const readingOverlay = document.getElementById("lss_reading_mode_overlay")
+    readingOverlay.remove();
+}
+
+function checkToggle(){
+    const readingOverlay = document.getElementById("lss_reading_mode_overlay")
+    if (readingOverlay === null){
+        return false;
+    }
+    return true;
+}
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    if (request.action === "displayOverlay"){
+        displayOverlay();
+        sendResponse({status: "success"})
+    } else if (request.action === "hideOverlay"){
+        destroyOverlay();
+        sendResponse({status: "success"})
+    } else if (request.action === "checkToggle"){
+        const hasOverlay = checkToggle();
+        sendResponse({
+            status: "success",
+            has_overlay: hasOverlay
+        })
+    }
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,4 +1,3 @@
-// popup.js
 function readTranslations(){
     const userLanguage = navigator.language;
     const filePath = "_locales/" + userLanguage + "/messages.json"
@@ -23,7 +22,41 @@ function translateContent(translations){
     document.getElementById("lss_reading_mode_active_title").innerText = translations['activate_reading_mode']['message']
 }
 
+function checkSwitchChecked(){
+    const toggleElement = document.getElementById("lss_reading_mode_toggle_switch");
+    getActiveTab().then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id,
+                {
+                    action: "checkToggle"
+                }).then((response) => {
+                    if (response.has_overlay){
+                        toggleElement.checked = true;
+                    } else {
+                        toggleElement.checked = false;
+                    }
+                console.log(response)
+            });
+        });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   readTranslations();
+  checkSwitchChecked()
 });
+
+function getActiveTab() {
+    return browser.tabs.query({active: true, currentWindow: true});
+}
+
+function onSwitchToggle(){
+    const toggleElement = document.getElementById("lss_reading_mode_toggle_switch");
+    getActiveTab().then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id,
+                {
+                    action: (toggleElement.checked) ? "displayOverlay": "hideOverlay"
+                }
+            );
+        });
+}
+document.getElementById("lss_reading_mode_toggle_switch").addEventListener("click", onSwitchToggle);
 
