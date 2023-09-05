0e0d659c79ea922a69f00a32a7c084dcc0f93064
George Papadopoulos <70651728+gpapadopp@users.noreply.github.com>
2023-09-05 19:24:18 +0300

    Added translations, title, subtitle, and toggle button.

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,13 +1,29 @@
 // popup.js
+function readTranslations(){
+    const userLanguage = navigator.language;
+    const filePath = "_locales/" + userLanguage + "/messages.json"
+    fetch(filePath)
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Error !")
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+          translateContent(jsonData)
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+}
 
-function updatePopupContent() {
-  browser.extension.getLocalizedUserMessage("extension_name", function(message) {
-      console.log(message)
-//    document.getElementById("welcomeMessage").textContent = message;
-  });
+function translateContent(translations){
+    document.getElementById("lss_title").innerText = translations['extension_name']['message']
+    document.getElementById("lss_subtitle").innerText = translations['subtitle_text']['message']
+    document.getElementById("lss_reading_mode_active_title").innerText = translations['activate_reading_mode']['message']
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  updatePopupContent();
+  readTranslations();
 });
 
